da7e7fdfd0a054d914ba4cd00a3f0e42677cf300
Alessandro Bianco <code@alebianco.dev>
2023-11-20 23:35:29 +0100

    🌟 2015 day 4 part 2

---
+++ editions/2015/src/04/part2.js
@@ -0,0 +1,20 @@
+import { isMain, PuzzleInput } from '@advent-of-code/utils'
+import { createHash } from 'node:crypto'
+
+const md5 = data => createHash('md5').update(data).digest('hex')
+
+const input = new PuzzleInput('input.txt', import.meta.url).firstLine()
+
+export function solve () {
+  let answer = 0
+  let hash
+  do {
+    hash = md5(`${input}${++answer}`)
+  } while (!hash.startsWith('000000'))
+
+  return answer
+}
+
+if (isMain(import.meta)) {
+  console.log(solve())
+}
