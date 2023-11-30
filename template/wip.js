3f6017b8837071319dca70b1b13f1c3d47012002
Alessandro Bianco <code@alebianco.dev>
2023-11-30 16:43:40 +0100

    🥛 add solution template

---
+++ template/wip.js
@@ -0,0 +1,12 @@
+import { isMain, PuzzleInput } from '@advent-of-code/utils'
+
+const input = new PuzzleInput('input.txt', import.meta.url)
+input.lines()
+
+export function solve () {
+  return undefined
+}
+
+if (isMain(import.meta)) {
+  console.log(solve())
+}
