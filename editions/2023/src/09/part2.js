ef6314fc20586be1e72b57b08ce19a23b6f3bf67
Alessandro Bianco <code@alebianco.dev>
2023-12-11 19:24:37 +0100

    🌟 2023 day 9 part 2

---
+++ editions/2023/src/09/part2.js
@@ -0,0 +1,25 @@
+import { isMain, PuzzleInput } from '@advent-of-code/utils'
+import { pascalRow } from '@advent-of-code/utils/numbers'
+
+const input = new PuzzleInput('input.txt', import.meta.url)
+
+function nextNumber (line) {
+  let sum = 0
+  for (let l = line.length; l > 0; l--) {
+    const coeff = pascalRow(l - 1)
+    const nextLevel = line.slice(line.length - l).reduce((tot, term, i) => -1 * tot + term * coeff[i], 0)
+    sum += nextLevel
+  }
+  return sum
+}
+
+export function solve () {
+  return input.lines()
+    .map(line => line.split(' ').map(Number).reverse())
+    .map(nextNumber)
+    .reduce((tot, term) => tot + term)
+}
+
+if (isMain(import.meta)) {
+  console.log(solve())
+}
