d81cc45593c6b53d5efbd81756e517c139f39473
Alessandro Bianco <code@alebianco.dev>
2023-12-13 23:50:37 +0100

    🌟 2023 day 13 part 1

---
+++ editions/2023/src/13/part1.js
@@ -0,0 +1,28 @@
+import { isMain, PuzzleInput } from '@advent-of-code/utils'
+
+const input = new PuzzleInput('input.txt', import.meta.url)
+
+function findReflection (grid) {
+  const lines = grid.lines()
+  for (let row = 1; row < lines.length; row++) {
+    const span = Math.min(row, lines.length - row)
+    const left = lines.slice(row - span, row).toString()
+    const right = lines.slice(row, row + span).reverse().toString()
+    if (left === right) {
+      return row
+    }
+  }
+  return 0
+}
+
+export function solve () {
+  let sum = 0
+  for (const section of input.sections()) {
+    sum += findReflection(section) * 100 || findReflection(section.transpose())
+  }
+  return sum
+}
+
+if (isMain(import.meta)) {
+  console.log(solve())
+}
