afa6c1b57b246a4735e1f56d05a112cf62e48ac3
Alessandro Bianco <code@alebianco.dev>
2023-12-13 23:50:20 +0100

    🎁 add levenshteinDistance to string utils

--- packages/utils/src/strings/index.js
+++ packages/utils/src/strings/index.js
@@ -1,5 +1,6 @@
 export * from './case-converter.js'
 export * from './count.js'
+export * from './levenshtein-distance.js'
 export * from './replace-at.js'
 export * from './translate.js'
 export * from './truncate.js'
