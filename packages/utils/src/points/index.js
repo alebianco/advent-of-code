b26b622721005f7541d2e680a1ae3202496079d6
Alessandro Bianco <code@alebianco.dev>
2023-12-17 20:15:57 +0100

    🎁 add point rotating matrices to points utils

--- packages/utils/src/points/index.js
+++ packages/utils/src/points/index.js
@@ -7,3 +7,27 @@ export * from './sum-point.js'
 /**
  * @typedef {[number, number]} Point2
  */
+
+// DIRECTIONS
+
+export const UP = [0, -1]
+export const DOWN = [0, 1]
+export const LEFT = [-1, 0]
+export const RIGHT = [1, 0]
+export const UP_RIGHT = [1, -1]
+export const DOWN_RIGHT = [1, 1]
+export const UP_LEFT = [-1, -1]
+export const DOWN_LEFT = [-1, 1]
+
+// GROUPS
+
+export const SQUARED = [UP, RIGHT, DOWN, LEFT]
+export const DIAGONALS = [UP_RIGHT, DOWN_RIGHT, DOWN_LEFT, UP_LEFT]
+export const ALL = [UP, UP_RIGHT, RIGHT, DOWN_RIGHT, DOWN, DOWN_LEFT, LEFT, UP_LEFT]
+
+// TURNS
+
+export const TURN_NONE = [[1, 0], [0, 1]]
+export const TURN_RIGHT = [[0, -1], [1, 0]]
+export const TURN_LEFT = [[0, 1], [-1, 0]]
+export const TURN_BACK = [[-1, 0], [0, -1]]
